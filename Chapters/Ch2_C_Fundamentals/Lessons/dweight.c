4f9990ac7f7d355532b2df205f0d54525aea5240
psitaraman <psitaraman1618@gmail.com>
2020-08-10 22:36:45 -0700

    ch2 dweight

---
+++ Chapters/Ch2_C_Fundamentals/Lessons/dweight.c
@@ -0,0 +1,31 @@
+
+// Computes the dimensional weight of a 12" x 10" x 8" box
+
+#include <stdio.h>
+
+int main(void) {
+
+	int height = 8;
+	int width = 10;
+	int length = 12;
+	int volume = height * width * length;
+	int weight = (volume + 165) / 166;
+	float price = weight * 1.49; // $1.49/lb
+
+	printf("Dimensions: %dx%dx%d\n", length, width, height);
+	printf("Volume (cubic inches): %d\n", volume);
+	printf("Dimensional weight (pounds): %d\n", weight);
+	printf("Shipping Price: $%.2f\n", price);
+
+	return 0;
+}
+
+// 	gcc -o dweight dweight.c
+// 	./dweight
+
+/* 	Output:
+	Dimensions: 12x10x8
+	Volume (cubic inches): 960
+	Dimensional weight (pounds): 6
+	Shipping Price: $8.94
+*/
---
+++ Chapters/Ch2_C_Fundamentals/Lessons/pun.c
@@ -0,0 +1,95 @@
+#include <stdio.h> //includes info on standard C input/output libraries
+
+// main program with no inputs and return type int
+int main(void) {
+	// print desired message with new line at the end
+	printf("To C, or not to C: that is the question.\n");
+	return 0;
+}
+
+// *** Note: `n` notation, where n can be anything, is used to highlight key terms
+/* The following steps are done when running a C program:
+
+	1. Preprocessing - program is given to a `preprocessor`
+		which computes instructions begining with `#`. 
+		Instructions beginning with `#` are called `directives`
+		This stage is called `preprocessing` because it modifies code 
+		based on the `directives` before the `compilation` step can proceed.
+
+	2. Compiling - The modified program is then fed to a `compiler` which 
+		`compiles` the human readable code into machine code instructions 
+		called `object code`.
+		
+	3. Linking - The compiled `object code` is then combined with additional files
+		such as libraries, other `object code`, media resources etc. using a `linker`. 
+		The `linker` is responsible for `linking` all files and resources needed to 
+		create a working executable program. 
+		In C programs usually a `MAKE` file is used to facilitate and automate the
+		execution of the `linking` of various files in order to create the executable. 
+
+	For compilation and linking with Linux systems, use `gcc`
+	For compilation and linking with UNIX systems, use `cc`
+	For compilation and linking with Mac OSX, use `cc` or `gcc`
+
+	Compile and Link Command:
+	% cc pun.c     
+	-> generates output file with default name `a.out`
+
+	% cc -o pun pun.c
+	-> generates output file with name `pun`
+	
+	Run output file
+	% ./a.out
+	% ./pun
+	-> To C, or not to C: that is the question.
+
+Simple C programs have the form:
+
+Directives
+
+int main(void) {
+	
+	declarations
+
+	statments
+
+}
+
+Directives - editing commands used to indicate places for program modification 
+				 prior to the onset of the compilation phase.
+				 Example: #include <stdio.h>
+				 		  This directive states that the information in 
+				 		  `<stdio.h>` (a standard I/O library) should be 
+				 		  included prior to program compilation.
+				 		  There is no ; or special marker at the end of 
+				 		  directives. This is unlike most other C statements 
+				 		  where ; is required to indicate statment termination.
+
+Functions - subroutines used to encapsulate logical statments. This 
+			encapsulation allows for reproduceability and maintainability of 
+			code. C programs are little more than a collection of functions.
+			The `main()` function is mandatory in C since it is the predefined 
+			entry point into the program.
+			Functions also may take in inputs/arguments and provide outputs. 
+			If no input/output wish to be provided/given then the respecitve 
+			input/output function type should be marked as `void`.
+			Example: void main(void) { return; }
+
+
+Arithmetic on `float` numbers are slower than arithmetic on `int` numbers.
+`float` can store much larger numbers than `int`.
+The value of a `float` number is often an approximation. This is common
+with irrational number like 0.99999... which will be approximated to 
+0.99999..87
+
+Variable declaration:
+int height;
+float profit;
+
+It is best to append the letter `f` (for float) to a constant that 
+contains a decimal point.
+float profit = 2150.48f;
+
+*/
+
+
